42d11c99d140b3c175b2d401766960074e1b2800
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-08-05 05:26:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,13 @@ class Song
     song.name = title
     song
   end
-  
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = title
+    song
+  end
+
   def self.all
     @@all
   end
